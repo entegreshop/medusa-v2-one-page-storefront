c93f207b1585753914cef684df8614e9b2503909
entegreshop <modaskop@gmail.com>
2026-06-24 00:31:14 +0300

    fix(storefront): add rewrites for uploads

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/uploads/:path*',
+        destination: 'http://204.168.136.196:9001/uploads/:path*',
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
