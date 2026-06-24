2a7ce1a6c8d86bedb0acddaf41b21402d884dad8
entegreshop <modaskop@gmail.com>
2026-06-25 01:46:24 +0300

    feat: setup reverse proxy for medusa admin and update paths

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,26 @@ const nextConfig: NextConfig = {
         source: '/uploads/:path*',
         destination: 'http://204.168.136.196:9001/uploads/:path*',
       },
+      {
+        source: '/admin/:path*',
+        destination: 'http://204.168.136.196:9001/admin/:path*',
+      },
+      {
+        source: '/app/:path*',
+        destination: 'http://204.168.136.196:9001/app/:path*',
+      },
+      {
+        source: '/store/:path*',
+        destination: 'http://204.168.136.196:9001/store/:path*',
+      },
+      {
+        source: '/auth/:path*',
+        destination: 'http://204.168.136.196:9001/auth/:path*',
+      },
+      {
+        source: '/static/:path*',
+        destination: 'http://204.168.136.196:9001/static/:path*',
+      },
     ];
   },
   images: {
