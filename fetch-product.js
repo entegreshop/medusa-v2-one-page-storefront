62ad5a3996b84d853dbc68073fe081fa0319207f
entegreshop <modaskop@gmail.com>
2026-07-28 03:21:35 +0300

    chore: remove test scripts that cause nixpacks build failure

--- fetch-product.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-fetch('https://api.cizgibutik.com/store/products?handle=gold-dugmeli-denim-elbise')
-  .then(r => r.json())
-  .then(d => {
-    const p = d.products[0];
-    console.log('Options:', JSON.stringify(p.options, null, 2));
-    console.log('Variants:', JSON.stringify(p.variants.map(v => ({ id: v.id, title: v.title, options: v.options })), null, 2));
-  });
